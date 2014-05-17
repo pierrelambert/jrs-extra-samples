51fba6108333bf126cf14b395feb211de41101c6
François Cerbelle <francois@cerbelle.net>
2014-05-17 19:45:20 +0200

    Pierre's GMAP sample fixes and Install.sh update (cf full description) - Renammed the mysql dump to match the schema name - Added DROP DATABASE in the MySQL dump to make it idempotent - Added InstallFiles function to deal with binary files to deploy - Added ImportMySQL to import mysql dumps and give permissions to   jasperdb/password

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 BUILDOMATIC="/root/jasperreports-server-5.5-bin/buildomatic/"
+TOMCATROOT="/var/lib/tomcat6/"
 
 function Init(){ # Initializes the samples repository
 aptitude -y update \
@@ -43,11 +44,36 @@ echo "Importing ${1} MongoDB database..."
 mongorestore --drop /jrs-extra-samples/${1}/database/mongodb/
 }
 
+function MySQLRestore(){ # Import a mySQL dump for the specified sample (drops and recreates)
+[ ! -d /jrs-extra-samples ] && echo "Samples repository not initialized, please 'init' first." && exit 1
+[ ! -d /jrs-extra-samples/${1}/database/mysql/ ] && echo "No MySQL database to import" && return
+echo "Importing ${1} MySQL database..."
+for dump in /jrs-extra-samples/${1}/database/mysql/*.sql; do
+    mysql -h localhost -uroot -proot < ${dump}
+    echo "GRANT ALL PRIVILEGES ON \`states\`.* TO 'jasperdb'@'%' IDENTIFIED BY 'password';" | mysql -h localhost -uroot -proot
+    echo "GRANT ALL PRIVILEGES ON \`states\`.* TO 'jasperdb'@'localhost' IDENTIFIED BY 'password';" | mysql -h localhost -uroot -proot
+done
+}
+
+function InstallFiles(){ # Installs files in tomcat's root
+[ ! -d /jrs-extra-samples ] && echo "Samples repository not initialized, please 'init' first." && exit 1
+[ ! -d /jrs-extra-samples/${1}/filesystem/ ] && echo "No file to install" && return
+echo "Installing ${1} files..."
+cd /jrs-extra-samples/${1}/filesystem
+for file in `find ./ -type f`; do
+    mkdir -p ${TOMCATROOT}/${file}
+    rm -Rf ${TOMCATROOT}/${file}
+    cp ${file} ${TOMCATROOT}/${file}
+done
+}
+
 function Install(){ # Installs a specific sample (Repo, DB, ...)
 [ ! -d /jrs-extra-samples ] && echo "Samples repository not initialized, please 'init' first." && exit 1
 [ ! -d /jrs-extra-samples/${1}/ ] && echo "Sample ${1} not found" && return
 JRSImport  ${1}
 MongoRestore ${1}
+MySQLRestore ${1}
+InstallFiles ${1}
 }
 
 function Usage(){ # Help message
